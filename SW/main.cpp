792030adfb18edd960ea0dbbc5286be552bb4264
BorysMakowski <bormak@wp.pl>
2023-03-09 21:39:42 +0100

    edit main

--- SW/main.cpp
+++ SW/main.cpp
@@ -4,27 +4,17 @@ int main()
 {
 	std::vector <std::string> wordList = propagateWordList();
 	reduceWordList(wordList, 3000);
-	std::ofstream results("results_100k_brutes.txt");
-	int timesToRun = 100000;
-
-	auto globalStart = std::chrono::system_clock::now();
-	//std::ofstream results("results.txt", std::ios_base::app); //append instead of overwrite
+	std::ofstream results("results.txt");
+	int timesToRun = 100;
 
 	test(new BruteSolver(wordList), results, timesToRun, wordList, false);
 	test(new BruteSolverNoRepeat(wordList), results, timesToRun, wordList, false);
-	//test(new SolverA(wordList), results, timesToRun, wordList, false);
-	//test(new SolverB(wordList), results, timesToRun, wordList, false);
-	//test(new SolverC(wordList), results, timesToRun, wordList, false);
-	//test(new SolverMatchPatternA(wordList), results, timesToRun, wordList, false);
-	//test(new SolverMatchPatternB(wordList), results, timesToRun, wordList, false);
-	//test(new SolverMatchPatternC(wordList), results, timesToRun, wordList, false);
+	test(new SolverA(wordList), results, timesToRun, wordList, false);
+	test(new SolverB(wordList), results, timesToRun, wordList, false);
+	test(new SolverC(wordList), results, timesToRun, wordList, false);
+	test(new SolverMatchPatternA(wordList), results, timesToRun, wordList, false);
+	test(new SolverMatchPatternB(wordList), results, timesToRun, wordList, false);
+	test(new SolverMatchPatternC(wordList), results, timesToRun, wordList, false);
 
-	auto globalEnd = std::chrono::system_clock::now();
-	std::chrono::duration<double> globalTime = globalEnd - globalStart;
-	results << "whole job took: " << globalTime.count() << " seconds" << std::endl;
 	results.close();
-
-	//play(wordList);
-	system("pause");
 }
-
